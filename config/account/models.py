12271cc5582dc8e708d61151c86a4675afad6732
javoh20 <javoh7806@gmail.com>
2024-09-07 16:48:04 +0500

    models

--- config/account/models.py
+++ config/account/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    description = models.CharField(max_length = 255, null = True)
+    photo = models.ImageField(upload_to = 'media/users/profile_photo', null = True)
+
+class UserSubscribers(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='user')
+    subscribers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscribers')
